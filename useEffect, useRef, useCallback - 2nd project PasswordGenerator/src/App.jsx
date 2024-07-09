e85ea73e1117d23969ae9d905069d5875a474a1d
Anurag Chaudhary Tharu <anuragchaudharytharu371@gmail.com>
2024-07-09 23:04:34 +0545

    2024-07-09 repo created and added my learning journey

---
+++ useEffect, useRef, useCallback - 2nd project PasswordGenerator/src/App.jsx
@@ -0,0 +1,111 @@
+import { useState, useCallback, useEffect, useRef } from 'react';
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState('');
+
+  // useRef() Hook
+  // used when we need to take reference of any element of the webpage
+  const passwordRef = useRef(null);
+
+  // useCallback() Hook
+  // useCallback(function, dependecies that is responsible for changes for optimization)
+  // use Callback: used for optimization it calls the function inside it when the dependencies are changed and returns a memorized function
+  const passwordGenerator = useCallback(() => {
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+    if (numberAllowed) {
+      str += '0123456789';
+    }
+
+    if (charAllowed) {
+      str += '!@#$%^&*-_+=[]{}~`';
+    }
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]); //setPassword is relation to password. So, setPassword is used for better optimization  //password instead of setPassword is not used because password keeps on changing by the loop run and its optimized again and again due to password change
+
+  // useEffect() Hook
+  // useEffect(callback function, dependecies that is responsible for changes-- it is not used for optimization)
+  // useEffect: runs the function inside it whenever the page renders for a first-time or whenever dependencies are changed
+  // we cannot call passwordGenerator() due to use of useCallback. We also can't do this even if there is no useCallback() hook
+  // for this we use useEffect() hook
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 101); //setSelectRange(start, end) what range to select upto //also select ending range // it should be at least 1 more than the required (here, 100 from input element) range
+    window.navigator.clipboard.writeText(password); //paste/write password inside window clipboard for copy functionality
+  }, [password]);
+
+  return (
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className="text-white text-center my-3">Password generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef} //give value to reference of passwordRef variable useRef()
+        />
+        <button
+          onClick={copyPasswordToClipboard}
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+        >
+          copy
+        </button>
+      </div>
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
